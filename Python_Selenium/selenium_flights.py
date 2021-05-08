199fed06eb18635796ca3e26afb0e184dbf8044a
Yaniv Bronshtein <yanbronshtein@gmail.com>
2021-05-07 23:22:05 -0400

    Finished Data Wrangling final project for submission

--- Python_Selenium/selenium_flights.py
+++
@@ -1,6 +0,0 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-
-driver = webdriver.Firefox('/Users/yanivbronshtein/Coding/Rutgers/DataWrangling_FinalProject/Python_Selenium/')
-driver.get("http://www.python.org")
-driver.close()
